21537f6511cbfaa23fc4bbe242e78af2f1f2cf3c
Andrew Ferrier <andrewferrier@example.com>
2015-04-10 22:04:20 -0500

    Support checking for --help.

---
+++ tests/Subprocess/test_Subprocess_Arguments.py
@@ -0,0 +1,15 @@
+import os
+
+from freezegun import freeze_time
+from tests.BaseTestClasses import NormalizeFilenameTestCase
+
+
+class TestSubprocessArguments(NormalizeFilenameTestCase):
+    def setUp(self):
+        super(TestSubprocessArguments, self).setUp()
+
+    def test_no_basicdateprefix(self):
+        (rc, output, error) = self.invokeAsSubprocess([], extraParams=['--help'], expectOutput=True)
+        self.assertEqual(0, rc)
+        self.assertEqual(0, self.directoryCount(self.workingDir))
+        self.assertEqual('', error)
---
+++ tests/Subprocess/test_Subprocess_Errors.py
@@ -0,0 +1,14 @@
+import os
+
+from freezegun import freeze_time
+from tests.BaseTestClasses import NormalizeFilenameTestCase
+
+class TestSubprocessErrors(NormalizeFilenameTestCase):
+    def setUp(self):
+        super(TestSubprocessErrors, self).setUp()
+
+    def test_no_files(self):
+        (rc, output, error) = self.invokeAsSubprocess([], expectOutput=True)
+        self.assertEqual(2, rc)
+        self.assertEqual(0, self.directoryCount(self.workingDir))
+        self.assertRegex(error, ".*You.*must.*specify.*some.*")
