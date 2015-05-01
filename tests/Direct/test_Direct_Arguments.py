ba733cfbd53247b8d516877a86563f910f7455cf
Andrew Ferrier <andrewferrier@example.com>
2015-05-01 18:03:17 -0500

    Add --force option for overwrites - closes #21

--- tests/Direct/test_Direct_Arguments.py
+++ tests/Direct/test_Direct_Arguments.py
@@ -43,3 +43,14 @@ class TestDirectArguments(NormalizeFilenameTestCase):
         self.assertTrue(os.path.exists(os.path.join(self.workingDir, self.getDateAndTimePrefix() + 'blah.txt')))
         self.assertEqual(1, self.directoryFileCount(self.workingDir))
         self.assertEqual('', error)
+
+    def test_targetfile_exists_with_force(self):
+        filename = os.path.join(self.workingDir, 'blah.txt')
+        self.writeFile(filename, "original")
+        filename2 = os.path.join(self.workingDir, self.getDatePrefix() + 'blah.txt')
+        self.writeFile(filename2, "new")
+        self.invokeDirectly([filename], extraParams=['--force'])
+        self.assertFalse(os.path.exists(filename))
+        self.assertTrue(os.path.exists(filename2))
+        self.assertEqual(self.readFile(filename2), "original")
+        self.assertEqual(1, self.directoryFileCount(self.workingDir))
