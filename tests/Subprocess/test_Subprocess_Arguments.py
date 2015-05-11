d8196e8db5b08bbd99296278c47def7287ad9f4b
Andrew Ferrier <andrewferrier@example.com>
2015-05-11 16:48:30 -0500

    Ensured there is at least one test for each CLI option - closes #6

--- tests/Subprocess/test_Subprocess_Arguments.py
+++ tests/Subprocess/test_Subprocess_Arguments.py
@@ -1,3 +1,5 @@
+import os.path
+
 from tests.BaseTestClasses import NormalizeFilenameTestCase
 
 
@@ -10,3 +12,14 @@ class TestSubprocessArguments(NormalizeFilenameTestCase):
         self.assertEqual(0, rc)
         self.assertEqual(0, self.directoryFileCount(self.workingDir))
         self.assertEqual('', error)
+
+    def test_verbose(self):
+        filename = os.path.join(self.workingDir, 'blah.txt')
+        self.touch(filename)
+        (rc, output, error) = self.invokeAsSubprocess([filename], extraParams=['--verbose'])
+        self.assertEqual(0, rc)
+        self.assertFalse(os.path.exists(filename))
+        self.assertTrue(os.path.exists(os.path.join(self.workingDir, self.getDatePrefix() + 'blah.txt')))
+        self.assertEqual(1, self.directoryFileCount(self.workingDir))
+        self.assertEqual('', output)
+        self.assertRegex(error, 'moved to')
