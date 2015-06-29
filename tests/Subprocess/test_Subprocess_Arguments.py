8f4763411a2bccdd7c403609aa953f2fec147274
Andrew Ferrier <andrewferrier@example.com>
2015-06-29 13:05:04 -0500

    Handle current directory with recursion.

--- tests/Subprocess/test_Subprocess_Arguments.py
+++ tests/Subprocess/test_Subprocess_Arguments.py
@@ -23,3 +23,16 @@ class TestSubprocessArguments(NormalizeFilenameTestCase):
         self.assertEqual(1, self.directoryFileCount(self.workingDir))
         self.assertEqual('', output)
         self.assertRegex(error, 'moved to')
+
+    def test_recursive_current_directory(self):
+        subWorkingDir = os.path.join(self.workingDir, 'subWorkingDir')
+        filename = os.path.join(subWorkingDir, 'foobar.txt')
+        self.touch(filename)
+        (rc, output, error) = self.invokeAsSubprocess(['.'], extraParams=['--recursive'], cwd=subWorkingDir)
+        self.assertEqual(0, rc)
+        self.assertEqual('', error)
+        newsubWorkingDir = os.path.join(self.workingDir, self.getDatePrefix() + 'subWorkingDir')
+        self.assertPathDoesntExist(filename)
+        self.assertPathDoesntExist(subWorkingDir)
+        self.assertPathExists(os.path.join(newsubWorkingDir, self.getDatePrefix() + 'foobar.txt'))
+        self.assertEqual(1, self.directoryFileCount(newsubWorkingDir))
