e5994c7d1ebf99df0104aea8d17d2e75d5c58263
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2023-11-26 12:45:48 +0000

    test: Add more verbosity

--- tests/Subprocess/test_Subprocess_Arguments.py
+++ tests/Subprocess/test_Subprocess_Arguments.py
@@ -39,7 +39,7 @@ class TestSubprocessArguments(NormalizeFilenameTestCase):
         subWorkingDir = os.path.join(self.workingDir, 'subWorkingDir')
         filename = os.path.join(subWorkingDir, 'foobar.txt')
         self.touch(filename)
-        (rc, output, error) = self.invokeAsSubprocess(['.'], extraParams=['--recursive'], cwd=subWorkingDir)
+        (rc, output, error) = self.invokeAsSubprocess(['.'], extraParams=['--recursive', '-vv'], cwd=subWorkingDir)
         print(f"DEBUGPRINT[2]: test_Subprocess_Arguments.py:42: error={error}")
         print(f"DEBUGPRINT[1]: test_Subprocess_Arguments.py:42: output={output}")
         self.assertEqual(0, rc)
